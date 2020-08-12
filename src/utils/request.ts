809dc68d0f28b73ba327c901a7db089da838ce6f
sonamu.liao <sonamu@liaoqingsong.com>
2020-08-12 16:29:27 +0800

    add error toast

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import Incorrect from 'classes/Incorrect';
 import { errorCode } from 'configs/enumerations';
+import { message } from 'antd';
 
 interface RequestModel extends AxiosRequestConfig {
   /** 默认错误会进行 `message.error` 提示，是否禁用 */
@@ -24,6 +25,7 @@ export default function request <T> ({ disableErrorMessage, ...option }: Request
       throw new Incorrect(data.code, data.message ?? errorCode[data.code]?.label ?? errorCode.default.label);
     }).catch(e => {
       const error = e instanceof Incorrect ? e : new Incorrect(errorCode.default.code, e?.message ?? e ?? errorCode.default.label);
+      !disableErrorMessage && message.error(error.messge);
       // TODO 部分特殊 `code` 处理
       throw error;
     });
