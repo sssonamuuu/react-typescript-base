a980cd4528755da9c35255ca68dcb8e34b791564
sonamu.liao <sonamu@liaoqingsong.com>
2020-08-19 16:10:51 +0800

    base form

---
+++ src/components/BaseForm/BaseFormItem/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Form } from 'antd';
+import { FormItemProps } from 'antd/lib/form';
+
+export interface BaseFormInstance<T> {
+  /** 获取某个字段的值 */
+  getFieldValue: <K extends keyof T> (name: K) => T[K];
+  /** 获取所有字段的值 */
+  getFieldsValue: () => T;
+  /** 获取某个字段的校验错误信息 */
+  getFieldError: <K extends keyof T>(name: K) => string[];
+  /** 获取某些字段的校验错误信息 */
+  getFieldsError: <K extends keyof T>(nameList?: K[]) => string[];
+  /** 重置所有(不传参数)/部分字段 */
+  resetFields: <K extends keyof T>(fields?: K[]) => void;
+  /** 设置字段 */
+  setFieldsValue: (value: Partial<T>) => void;
+  /** 校验所有(不传参数)/部分字段 */
+  validateFields: <K extends keyof T>(names?: K[]) => Promise<T>;
+}
+
+interface BaseFormItemProps<T> extends Omit<FormItemProps, 'name'> {
+  name?: keyof T;
+  children?: React.ReactNode | ((form: BaseFormInstance<T>) => React.ReactNode);
+}
+
+export default function BaseFormItem<T> (props: BaseFormItemProps<T>) {
+  return <Form.Item {...props as unknown as FormItemProps} />;
+}
+
--- src/components/BaseForm/index.tsx
+++ src/components/BaseForm/index.tsx
@@ -1,39 +1,14 @@
 import * as React from 'react';
-import Form, { FormProps, FormItemProps } from 'antd/lib/form';
-
-interface BaseFormInstance<T> {
-  /** 获取某个字段的值 */
-  getFieldValue: <K extends keyof T> (name: K) => T[K];
-  /** 获取所有字段的值 */
-  getFieldsValue: () => T;
-  /** 获取某个字段的校验错误信息 */
-  getFieldError: <K extends keyof T>(name: K) => string[];
-  /** 获取某些字段的校验错误信息 */
-  getFieldsError: <K extends keyof T>(nameList?: K[]) => string[];
-  /** 重置所有(不传参数)/部分字段 */
-  resetFields: <K extends keyof T>(fields?: K[]) => void;
-  /** 设置字段 */
-  setFieldsValue: (value: Partial<T>) => void;
-  /** 校验所有(不传参数)/部分字段 */
-  validateFields: <K extends keyof T>(names?: K[]) => Promise<T>;
-}
+import Form, { FormProps } from 'antd/lib/form';
+import BaseFormItem, { BaseFormInstance } from './BaseFormItem';
 
 export interface BaseFormProps<T> extends Omit<FormProps, 'form'> {
   initialValues?: T;
   form?: BaseFormInstance<T>;
 }
 
-interface BaseFormItemProps<T> extends Omit<FormItemProps, 'name'> {
-  name?: keyof T;
-  children?: React.ReactNode | ((form: BaseFormInstance<T>) => React.ReactNode);
-}
-
 export default function BaseForm <T> (props: BaseFormProps<T>) {
   return <Form {...props as unknown as FormProps} />;
-}
-
-function BaseFormItem<T> (props: BaseFormItemProps<T>) {
-  return <Form.Item {...props as unknown as FormItemProps} />;
 }
 
 BaseForm.useForm = Form.useForm as unknown as <T>() => [BaseFormInstance<T>];
