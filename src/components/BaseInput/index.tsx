d20f80f6117f987d8b0dd8e6c8ba21670effd876
sonamu.liao <sonamu@liaoqingsong.com>
2020-08-25 15:09:35 +0800

    base input

---
+++ src/components/BaseInput/index.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { Input } from 'antd';
+import { InputProps } from 'antd/lib/input';
+import { LoadingOutlined, SearchOutlined } from '@ant-design/icons';
+
+interface BaseInputProps extends Omit<InputProps, 'onChange'> {
+  value?: string;
+  valueType?: 'number' | '';
+  /**
+   * 是否去掉首尾空格，默认：`true`
+   */
+  /**
+   * 如果是 number decimal 表示小数精度
+   */
+  decimal?: number;
+  trim?: boolean;
+  search?: boolean;
+  onSearch?(value?: string): void;
+  onChange?(value?: string): void;
+  /** 仅在search下生效 */
+  loading?: boolean;
+}
+
+export default function BaseInput ({ value, onChange, valueType, decimal = 0, search = false, onSearch, loading = false, trim = true, ...props }: BaseInputProps) {
+  const lastValue = React.useRef(value);
+  const [currentValue, setCurrentValue] = React.useState(lastValue.current);
+
+  React.useEffect(() => setCurrentValue(value), [value]);
+
+  function calcValue (value: string = '') {
+    switch (valueType) {
+      case 'number':
+        const matches = value.match(new RegExp(`\\d+${decimal ? `(\\.\\d{0,${decimal}})?` : ''}`));
+        return matches?.[0] ?? '';
+      default: return value;
+    }
+  }
+
+  React.useEffect(() => {
+    lastValue.current !== currentValue && onChange?.(currentValue);
+    lastValue.current = currentValue;
+  }, [currentValue]);
+
+  function onChangeHandle (e: React.ChangeEvent<HTMLInputElement>) {
+    setCurrentValue(calcValue(e.target.value));
+  }
+
+  return (
+    <Input
+      autoComplete="off"
+      spellCheck={false}
+      suffix={search ? loading ? <LoadingOutlined /> : <SearchOutlined onClick={() => onSearch?.(currentValue)} /> : void 0}
+      onKeyDown={e => e.keyCode === 13 && onSearch?.(currentValue)}
+      onFocus={e => e.target.readOnly = false}
+      onBlur={e => trim && setCurrentValue(e.target.value.trim())}
+      {...props}
+      value={currentValue}
+      onChange={onChangeHandle} />
+  );
+}
+
+BaseInput.Group = Input.Group;
+BaseInput.TextArea = Input.TextArea;
