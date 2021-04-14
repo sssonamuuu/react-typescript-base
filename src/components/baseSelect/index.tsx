4b439404c6a70e12b5988eb7198c03c49fdf6afb
liaoqingsong <liaoqingsong@front-end.com.cn>
2021-04-14 17:16:17 +0800

    新增 select 全部

---
+++ src/components/baseSelect/index.tsx
@@ -0,0 +1,13 @@
+import { Select, SelectProps } from 'antd';
+import { SelectValue } from 'antd/lib/select';
+import React, { useMemo } from 'react';
+
+interface BaseSelectProps<T> extends Omit<SelectProps<T>, 'children'> {
+  /** 是否有全部选项，默认有， undefined | null 都认为是选中全部，如果选择全部返回值为 null */
+  all?: boolean;
+}
+
+export default function BaseSelect <T extends SelectValue = SelectValue> ({ value, all = true, options = [], ...props }: BaseSelectProps<T>) {
+  const memoOptions = useMemo(() => all ? [{ value: null, label: '全部' } as any].concat(options) : options, [all, options]);
+  return <Select {...props} options={memoOptions} value={value === void 0 ? null : value as any} />;
+}
