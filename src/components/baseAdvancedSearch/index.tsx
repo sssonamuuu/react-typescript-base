24d93a018a02a0e67d19c6b19578d35f8793af0e
liaoqingsong <liaoqingsong@front-end.com.cn>
2021-04-14 16:31:47 +0800

    onsearch

--- src/components/baseAdvancedSearch/index.tsx
+++ src/components/baseAdvancedSearch/index.tsx
@@ -8,8 +8,10 @@ import BaseCard, { BaseCardProps } from 'components/baseCard';
 
 interface BaseAdvancedSearchProps<T> extends BaseFormProps<T> {
   children?: ReactElement<BaseFormProps<T> & {label?: string}>[];
-  resetBtnProps?: ButtonProps;
-  searchBtnProps?: ButtonProps;
+  resetBtnProps?: Omit<ButtonProps, 'onClick'>;
+  searchBtnProps?: Omit<ButtonProps, 'onClick'>;
+  onSearch?(): void;
+  onReset?(): void;
 }
 
 export default function BaseAdvancedSearch <T> ({
@@ -17,11 +19,13 @@ export default function BaseAdvancedSearch <T> ({
   className = '',
   resetBtnProps: { ...resetBtnProps } = {},
   searchBtnProps: { className: searchBtnClassName = '', ...searchBtnProps } = {},
+  onSearch,
+  onReset,
   ...props
 }: BaseAdvancedSearchProps<T>) {
   return (
     <BaseForm {...props} className={`${style.searchBox} ${className}`}>
-      <Row gutter={5}>
+      <Row gutter={10}>
         {Children.map(children, child => child ? (
           <Col span={6}>
             {cloneElement(child)}
@@ -29,8 +33,8 @@ export default function BaseAdvancedSearch <T> ({
         ) : null)}
         <Col span={6}>
           <div className={style.searchCtrlBox}>
-            <Button {...resetBtnProps}>重置</Button>
-            <Button type="primary" className={`${globalStyle.ml10} ${searchBtnClassName}`} {...searchBtnProps}>查询</Button>
+            <Button {...resetBtnProps} onClick={onReset}>重置</Button>
+            <Button type="primary" className={`${globalStyle.ml10} ${searchBtnClassName}`} {...searchBtnProps} onClick={onSearch}>查询</Button>
           </div>
         </Col>
       </Row>
