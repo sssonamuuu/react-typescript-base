3a6297cbf9f63b515aadc386c97064e760cbcebc
liaoqingsong <liaoqingsong@front-end.com.cn>
2021-04-25 15:17:49 +0800

    baseTable新增hidden

--- src/components/baseTable/index.tsx
+++ src/components/baseTable/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Table } from 'antd';
 import { TableProps, ColumnType } from 'antd/lib/table';
 
 interface BaseColumnType<T> extends Omit<ColumnType<T>, 'dataIndex'> {
   dataIndex?: keyof T;
+  hidden?: boolean;
 }
 
 interface BaseColumnGroupType<T> extends Omit<BaseColumnType<T>, 'dataIndex'> {
-  // eslint-disable-next-line no-use-before-define
   children: BaseColumnsType<T>[];
+  hidden?: boolean;
 }
 
 type BaseColumnsType<T> = (BaseColumnGroupType<T> | BaseColumnType<T>)[];
@@ -18,6 +19,11 @@ interface BaseTableProps<T = unknown> extends Omit<TableProps<T>, 'children' | '
   columns?: BaseColumnsType<T>;
 }
 
-export default function BaseTable<T> (props: BaseTableProps<T>) {
-  return <Table {...props as any} />;
+function formatColumns<T> (columns: BaseColumnsType<T> = []): BaseColumnsType<T> {
+  return columns.reduce<BaseColumnsType<T>>((p, { hidden, ...c }) => hidden ? p : [...p, { ...c, children: 'children' in c && c.children.length ? formatColumns(c.children as any) : void 0 }], []);
+}
+
+export default function BaseTable<T> ({ columns, ...props }: BaseTableProps<T>) {
+  const _columns = useMemo(() => formatColumns(columns), [columns]);
+  return <Table {...props as any} columns={_columns} />;
 }
