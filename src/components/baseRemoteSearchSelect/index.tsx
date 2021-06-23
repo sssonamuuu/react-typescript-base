2ae87bf7fa450367ec52d679a7b410787e85cd70
liaoqingsong <liaoqingsong@front-end.com.cn>
2021-06-23 09:31:53 +0800

    remoteselect

--- src/components/baseRemoteSearchSelect/index.tsx
+++ src/components/baseRemoteSearchSelect/index.tsx
@@ -4,49 +4,67 @@ import { LoadingOutlined } from '@ant-design/icons';
 import { SelectValue } from 'antd/lib/select';
 
 export interface BaseRemoteSearchSelectProps<T> extends
-  Omit<SelectProps<T>, 'children' | 'options' | 'filterOption' | 'showSearch' | 'onSearch'> {
-  remoteLoadData?(value?: string): Promise<SelectProps<T>['options']>;
+  Omit<SelectProps<T>, 'children' | 'options' | 'filterOption' | 'showSearch' | 'onSearch' | 'onChange'> {
+  remoteLoadData?(search?: string): Promise<SelectProps<T>['options']>;
+  onChange? (value?: T): void;
 }
 
-export default function BaseRemoteSearchSelect <T extends SelectValue = SelectValue> ({ remoteLoadData, ...props }: BaseRemoteSearchSelectProps<T>) {
-  const [searching, setSearchIng] = useState(false);
+export default function BaseRemoteSearchSelect <T extends SelectValue = SelectValue> ({ remoteLoadData, value, onChange, ...props }: BaseRemoteSearchSelectProps<T>) {
+  const [searching, setSearching] = useState(false);
+  const [currentValue, setCurrentValue] = useState(value);
   const [options, setOptions] = useState<SelectProps<T>['options']>([]);
   const timmer = useRef<number>();
   const fetchId = useRef(0);
 
-  function loadData (value?: string) {
+  useEffect(() => {
+    setCurrentValue(value);
+  }, [value]);
+
+  function loadData (search?: string) {
     clearTimeout(timmer.current);
 
     if (searching) {
-      timmer.current = setTimeout(loadData, 500, value);
+      timmer.current = setTimeout(loadData, 500, search);
       return;
     }
     fetchId.current += 1;
     const current = fetchId.current;
     setOptions([]);
-    setSearchIng(true);
+    setSearching(true);
 
-    remoteLoadData?.(value).then(res => {
+    remoteLoadData?.(search).then(res => {
       current === fetchId.current && setOptions(res);
-      setSearchIng(false);
+      setSearching(false);
     });
   }
 
   useEffect(() => {
+    setSearching(false);
+    fetchId.current = 0;
     loadData();
+    setCurrentValue(void 0);
+    onChange?.(value);
     return () => {
       clearTimeout(timmer.current);
     };
   }, [remoteLoadData]);
 
+  function _onChange (value: T) {
+    setCurrentValue(value);
+    onChange?.(value);
+  }
+
   return (
     <Select
-      {...props}
       filterOption={false}
       showSearch
       suffixIcon={searching ? <LoadingOutlined /> : void 0}
       notFoundContent={searching ? <Spin size="small" /> : void 0}
       options={options}
-      onSearch={loadData} />
+      onSearch={loadData}
+      {...props}
+      value={currentValue}
+      onChange={_onChange} />
+
   );
 }
