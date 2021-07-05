e564b3b8da6ceb7c8d95f3cb3393047a38a7b889
拾肆 <liaoqingsong@front-end.com.cn>
2021-07-05 17:35:02 +0800

    fix(baseRemoteSearchSelect): 引入lodash.debounce优化并修复原有加载BUG

--- src/components/baseRemoteSearchSelect/index.tsx
+++ src/components/baseRemoteSearchSelect/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { SelectProps, Spin, Select } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 import { SelectValue } from 'antd/lib/select';
+import debounce from 'lodash/debounce';
 
 export interface BaseRemoteSearchSelectProps<T> extends
   Omit<SelectProps<T>, 'children' | 'options' | 'filterOption' | 'showSearch' | 'onSearch' | 'onChange'> {
@@ -13,41 +14,45 @@ export default function BaseRemoteSearchSelect <T extends SelectValue = SelectVa
   const [searching, setSearching] = useState(false);
   const [currentValue, setCurrentValue] = useState(value);
   const [options, setOptions] = useState<SelectProps<T>['options']>([]);
-  const timmer = useRef<number>();
+  const timmer = useRef<NodeJS.Timeout>();
+  const search = useRef('');
   const fetchId = useRef(0);
 
   useEffect(() => {
+    search.current = '';
     setCurrentValue(value);
   }, [value]);
 
-  function loadData (search?: string) {
-    clearTimeout(timmer.current);
-
-    if (searching) {
-      timmer.current = setTimeout(loadData, 500, search);
-      return;
-    }
+  const loadData = useMemo(() => debounce(() => {
     fetchId.current += 1;
     const current = fetchId.current;
     setOptions([]);
     setSearching(true);
 
-    remoteLoadData?.(search).then(res => {
-      current === fetchId.current && setOptions(res);
-      setSearching(false);
+    remoteLoadData?.(search.current).then(res => {
+      if (current === fetchId.current) {
+        setOptions(res);
+        setSearching(false);
+      }
     });
-  }
+  }, 800), []);
 
   useEffect(() => {
     setSearching(false);
     fetchId.current = 0;
-    loadData();
     setCurrentValue(void 0);
-    onChange?.(value);
+    onChange?.(void 0);
+    search.current = '';
+    loadData();
     return () => {
-      clearTimeout(timmer.current);
+      clearTimeout(timmer.current!);
     };
   }, [remoteLoadData]);
+
+  function onSearch (value: string) {
+    search.current = value;
+    loadData();
+  }
 
   function _onChange (value: T) {
     setCurrentValue(value);
@@ -61,7 +66,7 @@ export default function BaseRemoteSearchSelect <T extends SelectValue = SelectVa
       suffixIcon={searching ? <LoadingOutlined /> : void 0}
       notFoundContent={searching ? <Spin size="small" /> : void 0}
       options={options}
-      onSearch={loadData}
+      onSearch={onSearch}
       {...props}
       value={currentValue}
       onChange={_onChange} />
