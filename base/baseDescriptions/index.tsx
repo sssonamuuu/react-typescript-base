83866b277df4a2457dd16a189fb486307cb1c2e3
拾肆 <liaoqingsong@front-end.com.cn>
2021-07-27 14:16:48 +0800

    feat: description 组件新增tooltips

--- base/baseDescriptions/index.tsx
+++ base/baseDescriptions/index.tsx
@@ -1,13 +1,13 @@
-import style from './index.less';
+import style from './index.module.less';
 import React, { ReactNode } from 'react';
-import globalStyle from 'index.less';
-import { Row, Col } from 'antd';
+import globalStyle from 'index.module.less';
+import { Row, Col, Tooltip } from 'antd';
 import { isNullOrUndefined } from 'utils/dataTypeTools';
 
 type Layout = 'vertial' | 'horizontal';
 type Align = 'left' | 'center' | 'right';
 
-interface BaseDescriptionsItem {
+interface BaseDescriptionsItem extends Pick<BaseDescriptionsProps, 'ellipsis' | 'showTooltips'> {
   className?: string;
   labelClassName?: string;
   contentClassName?: string;
@@ -40,6 +40,20 @@ interface BaseDescriptionsProps {
   colon?: boolean;
   /** 带有副标题的缩进 */
   indent?: boolean;
+  /**
+   * 统一配置是否超出省略
+   *
+   * true 表示 1
+   *
+   * @default false
+   */
+  ellipsis?: boolean | 1 | 2 | 3;
+  /**
+    * 统一配置和是否显示tooltip的内容
+    *
+    * @default false/true(if ellipsis = true)
+    */
+  showTooltips?: boolean | React.ReactNode;
 }
 
 export default function BaseDescriptions ({
@@ -54,6 +68,8 @@ export default function BaseDescriptions ({
   defaultValue = '--',
   colon = true,
   indent,
+  ellipsis,
+  showTooltips,
 }: BaseDescriptionsProps) {
   return (
     <Row gutter={[10, 20]} className={`${className} ${indent ? globalStyle.px10 : ''}`}>
@@ -70,27 +86,43 @@ export default function BaseDescriptions ({
         labelWidth: itemLabelWidth = labelWidth,
         defaultValue: itemDefaultValue = defaultValue,
         colon: itemColon = colon,
-      }) => (
-        <Col
-          hidden={hidden}
-          style={{ flexDirection: itemLayout === 'horizontal' ? 'row' : 'column' }}
-          className={`${style.description} ${itemClassName}`}
-          span={itemSpan}
-          key={`${label}`}>
-          <span
-            className={`${style.label} c-base-description-label ${itemLabelClassName}`}
-            style={{
-              textAlign: itemLabelAlign,
-              width: itemLabelAlign === 'left' ? 'auto' : itemLabelWidth,
-            }}>
-            {label}
-            <span className={style.colon} style={{ visibility: itemColon ? 'visible' : 'hidden' }}>:</span>
+        ellipsis: itemEllipsis = ellipsis,
+        showTooltips: itemShowTooltips = showTooltips ?? !!itemEllipsis,
+      }) => {
+        const content = value === '' || isNullOrUndefined(value) ? itemDefaultValue : value;
+
+        const contentEl = (
+          <span className={`${style.content} ${itemEllipsis ? globalStyle[`toe${+itemEllipsis}`] : ''} ${itemContentClassName} ${itemLayout === 'horizontal' ? globalStyle.mt0 : globalStyle.mt10}`}>
+            {content}
           </span>
-          <span className={`${style.content} ${itemContentClassName} ${itemLayout === 'horizontal' ? globalStyle.mt0 : globalStyle.mt10}`}>
-            {value === '' || isNullOrUndefined(value) ? itemDefaultValue : value}
-          </span>
-        </Col>
-      ))}
+        );
+
+        const tooltipsEl = (
+          <Tooltip placement="topLeft" overlay={itemShowTooltips === true ? content : itemShowTooltips} overlayStyle={{ maxWidth: 450 }}>
+            {contentEl}
+          </Tooltip>
+        );
+
+        return (
+          <Col
+            hidden={hidden}
+            style={{ flexDirection: itemLayout === 'horizontal' ? 'row' : 'column' }}
+            className={`${style.description} ${itemClassName}`}
+            span={itemSpan}
+            key={`${label}`}>
+            <span
+              className={`${style.label} c-base-description-label ${itemLabelClassName}`}
+              style={{
+                textAlign: itemLabelAlign,
+                width: itemLabelAlign === 'left' ? 'auto' : itemLabelWidth,
+              }}>
+              {label}
+              <span className={style.colon} style={{ visibility: itemColon ? 'visible' : 'hidden' }}>:</span>
+            </span>
+            {itemShowTooltips ? tooltipsEl : contentEl}
+          </Col>
+        );
+      })}
     </Row>
   );
 }
