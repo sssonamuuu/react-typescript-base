82096a6dc60da7ca8ad4c79ebc723fc6794ada2e
拾肆 <liaoqingsong@front-end.com.cn>
2021-10-10 10:49:16 +0800

    fix: 使用hooks启用滚动条

--- layouts/default/index.tsx
+++ layouts/default/index.tsx
@@ -4,12 +4,15 @@ import globalStyle from 'index.less';
 import Header from './Header';
 import Menu from './Menu';
 import globalConfig from 'configs';
+import usePageConfig from 'hooks/usePageConfig';
 
 export default function LayoutPage ({ children }: PropsWithChildren<{}>) {
+  const { disablePageScroll, enablePageScroll } = usePageConfig({});
+
   /** 使用了 layout 使用局部滚动，隐藏全局滚动 */
   useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    () => document.body.style.overflow = 'auto';
+    disablePageScroll();
+    () => enablePageScroll();
   }, []);
 
   return (
